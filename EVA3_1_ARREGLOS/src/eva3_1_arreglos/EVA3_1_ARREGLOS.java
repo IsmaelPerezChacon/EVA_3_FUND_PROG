3ab25884494a4d0d1796383fccc54888373e540d
IsmaelPerezChacon <159494487+IsmaelPerezChacon@users.noreply.github.com>
2024-04-22 16:53:29 -0600

    Add files via upload

---
+++ EVA3_1_ARREGLOS/src/eva3_1_arreglos/EVA3_1_ARREGLOS.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_arreglos;
+
+import java.awt.BorderLayout;
+
+/**
+ *
+ * @author Jennis
+ */
+public class EVA3_1_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //los arrglos en java son objetos
+        //TENEMOS QUE CREARLOS ANTES DE USARLOS
+        // [] MANEJAR LOS INDICES ARREGLO [3] ACCESO A LA POSICION 
+        //CORCHETES SIRVEN PARA PONER EL INDICE PSICION A LA CUAL ACCEDER 
+        //ASI SE CREA EL ARREGLO, COMO CREAR OBJETOS EN JAVA ...... NEW....
+        //CREA UN ESPACIO EN LA MEMORIA
+        //Declaracion = creacion (new) [tamaño del arreglo]
+        int arreglo[] = new int [10]; // un arreglo de 10 enteros
+        //A TAVEZ DE indices;
+       // conocer las posiciones de valores;
+       //PRIMER POSICION DE UN ARREGLO ES 0 -----------D 0 
+       //ULTIMA POSICION DE UN ARREGLO ES ------D N - 1, DONDE
+       //N ES EL TAMAÑO DEL ARREGLO
+       arreglo [1] = 100;
+        System.out.println("Valor de arreglo [1] =" + arreglo[1]);
+        System.out.println(arreglo.length); // length es la cantidad de elementos 
+        
+                
+                }
+            }
+    
+
