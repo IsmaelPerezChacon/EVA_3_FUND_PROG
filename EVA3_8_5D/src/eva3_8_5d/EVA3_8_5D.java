5827a6ed093ef53a099414151b07194dc857210e
IsmaelPerezChacon <159494487+IsmaelPerezChacon@users.noreply.github.com>
2024-04-29 17:13:08 -0600

    Add files via upload

---
+++ EVA3_8_5D/src/eva3_8_5d/EVA3_8_5D.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_8_5d;
+
+/**
+ *
+ * @author Jennis
+ */
+public class EVA3_8_5D {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   int cubo [][][][][] = new int [10][5][3][2][1]; //150 ENTEROS
+        for(int i = 0; i < cubo.length; i++){
+          for(int j = 0; j < cubo[i].length; j++){
+            for(int k = 0; k < cubo[i][j].length; k++){
+                for(int m = 0; m< cubo[i][j][k].length; m++){
+            for(int n = 0; n < cubo[i][j][k][m].length; n++){
+                    cubo[i][j][k][m][n] = (int)(Math.random() * 100);
+                    
+            }
+          }
+        }
+          }
+        }
+             for(int i = 0; i < cubo.length; i++){
+          for(int j = 0; j < cubo[i].length; j++){
+            for(int k = 0; k < cubo[i][j].length; k++){
+                for(int m = 0; m< cubo[i][j][k].length; m++){
+            for(int n = 0; n < cubo[i][j][k][m].length; n++){
+                System.out.println("[" + cubo [i][j][k][m][n]+"]");
+            }
+                    
+    }
+          }
+             }
+    }
+  }
+        }
+    
